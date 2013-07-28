bf449faed00c1641f7ad30a243e9a116918a2ed4
Anton Podviaznikov <podviaznikov@gmail.com>
2013-07-27 22:55:37 -0500

    web app setup

--- project.clj
+++ project.clj
@@ -1,9 +1,18 @@
 (defproject jarkeeper.com "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://jarkeeper.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.taoensso/timbre "2.0.1"]
-                 [lein-ancient "0.4.3-SNAPSHOT"]
-                 [leiningen-core "2.2.0"]])
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.4"]
+                 [ring/ring-core "1.2.0"]
+                 [ring-server "0.2.8"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [ring/ring-jetty-adapter "1.2.0"]]
+  :ring {:handler jarkeeper.core/app }
+  :plugins [[lein-ring "0.8.6"]]
+  :profiles {
+    :dev {
+      :dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.0"]]}})
--- src/jarkeeper/core.clj
+++ src/jarkeeper/core.clj
@@ -1,6 +1,21 @@
-(ns jarkeeper.core)
+(ns jarkeeper.core
+  (:use
+    [ring.adapter.jetty :only [run-jetty]])
+  (:require [compojure.core :refer [defroutes GET POST PUT DELETE]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [hiccup.middleware :refer [wrap-base-url]]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [ring.util.response :as resp]
+            [clojure.tools.logging :as log]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes app-routes
+  (GET "/" [] "Hello"))
+
+
+(def app
+  (handler/site
+    app-routes))
+
+
+(defn -main [port] (run-jetty app {:port (Integer. port)}))
