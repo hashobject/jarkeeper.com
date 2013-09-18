73a306e17bfada0b4a909a400b72f2991b2538db
Anton Podviaznikov <podviaznikov@gmail.com>
2013-09-18 14:35:03 -0300

    progress

---
+++ src/jarkeeper/views/common.clj
@@ -0,0 +1,22 @@
+(ns jarkeeper.views.common
+  (:use [hiccup.core :only (html)]
+        [hiccup.page :only (html5 include-css include-js)])
+  (:require [clojure.string :as string]))
+
+(defn common-head [title]
+  '([:meta {:charset "utf-8"}]
+    [:title (str "Jarkeeper: " title)]
+    [:meta {:name "description" :content "Identifies out of date dependencies for Clojure projects on GitHub"}]
+    [:meta {:name "keywords" :content (str title ", clojure, dependecies, version, up to date version, out of date version")}]
+    [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
+    [:meta {:name "author" :content "Hashobject (team@hashobject.com)"}]
+    [:link {:rel "shortcut icon" :href "/favicon.ico"}]))
+
+
+(defn common-footer []
+  [:footer.row
+   [:div.small-12.columns
+    [:p "Jarkeeper brought to you by "
+      [:a {:href "http://hashobject.com/"} "Hashobject team"] "&mdash;2&nbsp;two people who love opensource."]
+    [:p.copyright "Hashobject, 2013"]]])
--- src/jarkeeper/views/project.clj
+++ src/jarkeeper/views/project.clj
@@ -1,5 +1,6 @@
 (ns jarkeeper.views.project
   (:require [clojure.string :as string]
+            [jarkeeper.views.common :as common-views]
             [hiccup.core :refer [html]]
             [hiccup.page :refer [html5 include-css include-js]]))
 
@@ -7,29 +8,32 @@
 (defn index [project]
   (html5 {:lang "en"}
     [:head
+     (common-views/common-head (:name project))
      (include-css "/app.css")]
-    [:body
+    [:body.project-page
       [:header.row
-       [:strong (:name project)]
-       [:span (:version project)]
-       [:p (:description project)]]
+       [:h1
+         [:a {:href (:github-url project)} (:name project)]
+         [:span.version (:version project)]]
+       [:h2 (:description project)]]
       [:section.summary.row
        [:ul
-        [:li (:total (:stats project)) " dependecies"]
-        [:li (:up-to-date (:stats project)) " up to date"]
-        [:li (:out-of-date (:stats project)) " out of date"]
+        [:li.small-12.large-3.columns (:total (:stats project)) " dependencies"]
+        [:li.small-12.large-3.columns (:up-to-date (:stats project)) " up to date"]
+        [:li.small-12.large-3.columns (:out-of-date (:stats project)) " out of date"]
         ]]
-      [:table
-        [:thead
-         [:tr
-          [:th "Dependecy"]
-          [:th "Used"]
-          [:th "Latest"]]]
-        (for [dep (:deps project)]
-            [:tr
-             [:td (first dep)]
-             [:td (second dep)]
-             [:td (str (first (last dep)))]])]
+      [:section.dependencies.row
+        [:table.small-12.columns
+          [:thead
+           [:tr
+            [:th "Dependecy"]
+            [:th "Used"]
+            [:th "Latest"]]]
+          (for [dep (:deps project)]
+              [:tr
+               [:td (first dep)]
+               [:td (second dep)]
+               [:td (str (first (last dep)))]])]]
 
-
+      (common-views/common-footer)
      ]))
