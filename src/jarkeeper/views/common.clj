92834f644f577189f08041bd05396f077def1c04
Anton Podviaznikov <podviaznikov@gmail.com>
2013-09-19 22:08:28 -0300

    minors

--- src/jarkeeper/views/common.clj
+++ src/jarkeeper/views/common.clj
@@ -10,6 +10,7 @@
     [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
     [:meta {:name "author" :content "Hashobject (team@hashobject.com)"}]
+    [:meta {:rel "author" :type "text/plain" :href "/humans.txt"}]
     [:link {:rel "shortcut icon" :href "/favicon.ico"}]))
 
 
