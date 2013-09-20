e638f62be969ca27b15c8966cf1c67b872c7eed7
Anton Podviaznikov <podviaznikov@gmail.com>
2013-09-19 22:29:25 -0300

    minors

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject jarkeeper.com "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Identify out of date dependecies in your Clojure project."
   :url "http://jarkeeper.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
