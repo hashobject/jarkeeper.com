8c89b2847c27b8bdb41b1ec89bb2c54303b7e77c
Anton Podviaznikov <podviaznikov@gmail.com>
2013-12-03 10:47:03 -0500

    set qualified? to false

--- src/jarkeeper/core.clj
+++ src/jarkeeper/core.clj
@@ -23,7 +23,7 @@
 
 (defn check-deps [deps]
   (map (fn [dep]
-         (conj dep (anc/artifact-outdated? {:snapshots? false :qualified? true} dep))
+         (conj dep (anc/artifact-outdated? {:snapshots? false :qualified? false} dep))
          ) deps))
 
 
