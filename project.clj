02d099fe3f790639333e2c1a3e45010a79b4ed05
Anton Podviaznikov <podviaznikov@gmail.com>
2014-04-28 23:43:59 +0200

    minors

--- project.clj
+++ project.clj
@@ -6,19 +6,19 @@
   :min-lein-version "2.0.0"
   :aws {
         :beanstalk {:region "us-west-1"}}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
-                 [ring/ring-core "1.2.1"]
+                 [ring/ring-core "1.2.2"]
                  [ring-server "0.3.1"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [ring/ring-jetty-adapter "1.2.1"]
-                 [ancient-clj "0.1.7"]]
+                 [ring/ring-jetty-adapter "1.2.2"]
+                 [ancient-clj "0.1.9"]]
   :ring {:handler jarkeeper.core/app }
   :plugins [[lein-ring "0.8.8"]
             [lein-beanstalk "0.2.7"]]
   :profiles {
     :dev {
-      :dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.1"]]}
+      :dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.2"]]}
     :prod {
       :ring {:open-browser? false, :stacktraces? false, :auto-reload? false}}})
