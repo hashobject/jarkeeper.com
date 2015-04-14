d7861c6a20cc0f2b46a79b99e7ba5436751b54a6
Maryna Aleksandrova <hello@marynaaleksandrova.info>
2015-04-14 15:06:01 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
+                 [compojure "1.3.3"]
                  [hiccup "1.0.5"]
                  [ring/ring-core "1.3.2"]
                  [ring-server "0.4.0"]
