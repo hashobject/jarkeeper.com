8e0d49ce8e9207dce4bd36c235aa5662d3ab18f5
Anton Podviaznikov <podviaznikov@gmail.com>
2015-09-03 22:35:48 -0700

    revert deps changes

--- project.clj
+++ project.clj
@@ -5,21 +5,21 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.4.0"]
+                 [compojure "1.3.3"]
                  [hiccup "1.0.5"]
-                 [ring/ring-core "1.4.0"]
+                 [ring/ring-core "1.3.2"]
                  [ring-server "0.4.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-jetty-adapter "1.3.2"]
                  [ancient-clj "0.3.11"]
-                 [ring/ring-json "0.4.0"]
+                 [ring/ring-json "0.3.1"]
                  [slingshot/slingshot "0.12.2"]]
   :ring {:handler jarkeeper.core/app }
-  :plugins [[lein-ring "0.9.6"]
+  :plugins [[lein-ring "0.9.2"]
             [jonase/eastwood "0.2.1"]]
   :profiles {
     :dev {
       :dependencies [[ring-mock "0.1.5"]
-                     [ring/ring-devel "1.4.0"]]}
+                     [ring/ring-devel "1.3.2"]]}
     :prod {
       :ring {:open-browser? false, :stacktraces? false, :auto-reload? false}}})
