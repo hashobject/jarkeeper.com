cfa5f7543da4842163761c85b7a5f89c36ff3f64
Anton Podviaznikov <podviaznikov@gmail.com>
2015-11-04 10:46:32 -0800

    add downloads badge

--- src/jarkeeper/core.clj
+++ src/jarkeeper/core.clj
@@ -9,6 +9,7 @@
             [clojure.tools.logging :as log]
             [clojure.edn :as edn]
             [clojure.java.io :as io]
+            [jarkeeper.downloads :as downloads]
             [jarkeeper.views.index :as index-view]
             [jarkeeper.views.project :as project-view]
             [jarkeeper.views.json :as project-json]
@@ -242,6 +243,23 @@
                (svg-status-resp "public/images/up-to-date.svg"))))
       (catch Exception e
         (rollbar/report-exception (env :rollbar-token) "production" e)
+        {:status 404})))
+
+  (GET "/:repo-owner/:repo-name/downloads.svg" [repo-owner repo-name]
+    (try
+      (do
+        ; (m/swap!
+        ;   (get-ref :downloads repo-owner repo-name)
+        ;   (fn [c] (if (nil? c) 1 (inc c))))
+        (-> (downloads/get-badge repo-owner repo-name)
+          (resp/response)
+          (resp/header "cache-control" "no-cache")
+          (resp/header "last-modified" (last-modified))
+          (resp/header "content-type" "image/svg+xml")
+
+       ))
+      (catch Exception e
+        ; (rollbar/report-exception (env :rollbar-token) "production" e)
         {:status 404})))
 
   (GET "/:repo-owner/:repo-name/status.json" [repo-owner repo-name]
