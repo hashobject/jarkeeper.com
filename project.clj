0e55c60a3879ad61bbfdcdde62aa72ff145fd86e
Anton Podviaznikov <anton@podviaznikov.com>
2017-11-13 21:21:02 -0800

    update deps

--- project.clj
+++ project.clj
@@ -12,17 +12,17 @@
                  [ring-server "0.5.0"]
                  [org.clojure/tools.logging "0.4.0"]
                  [ring/ring-jetty-adapter "1.6.3"]
-                 [ancient-clj "0.3.11"]
+                 [ancient-clj "0.6.14"]
                  [ring/ring-json "0.4.0"]
                  [slingshot/slingshot "0.12.2"]
-                 [environ "1.0.1"]
+                 [environ "1.1.0"]
                  [clj-rollbar "0.0.3"]
                  [matchbox "0.0.8-SNAPSHOT"]
-                 [clj-http "2.0.0"]]
+                 [clj-http "3.7.0"]]
   :main jarkeeper.core
   :ring {:handler jarkeeper.core/app }
   :plugins [[lein-ring "0.12.1"]
-            [jonase/eastwood "0.2.1"]
+            [jonase/eastwood "0.2.5"]
             ;[lein-kibit "0.1.2"]
             ]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"
@@ -31,6 +31,6 @@
     :dev {
       :ring {:open-browser? false}
       :dependencies [[ring-mock "0.1.5"]
-                     [ring/ring-devel "1.4.0"]]}
+                     [ring/ring-devel "1.6.3"]]}
     :prod {
       :ring {:open-browser? false, :stacktraces? false, :auto-reload? false}}})
